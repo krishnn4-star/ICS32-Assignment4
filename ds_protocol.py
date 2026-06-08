e1304b7e0126f58e34de1fd9f6da1194efc78606
krishnn4-star <krishnn4@uci.edu>
2026-06-07 17:06:55 -0700

    Add join message generation

--- ds_protocol.py
+++ ds_protocol.py
@@ -5,3 +5,12 @@ Response = namedtuple(
     "Response",
     ["type", "message", "token"]
 )
+
+def join(username, password):
+    return json.dumps({
+        "join": {
+            "username": username,
+            "password": password,
+            "token": ""
+        }
+    })
