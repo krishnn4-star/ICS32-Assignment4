863b55da3b3a926b5b28b6e95c40311083c6a97e
krishnn4-star <krishnn4@uci.edu>
2026-06-07 17:29:09 -0700

    Add DirectMessage class

--- ds_messenger.py
+++ ds_messenger.py
@@ -3,3 +3,10 @@ import time
 import ds_protocol
 
 PORT = 3001
+
+class DirectMessage:
+    def __init__(self):
+        self.recipient = None
+        self.sender = None
+        self.message = None
+        self.timestamp = None
