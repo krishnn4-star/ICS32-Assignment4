c59d7271e39d795e8e9baa847d60f213ee5f29c4
krishnn4-star <krishnn4@uci.edu>
2026-06-07 17:38:56 -0700

    Add server connection and join helper

--- ds_messenger.py
+++ ds_messenger.py
@@ -17,3 +17,21 @@ class DirectMessenger:
         self.username = username
         self.password = password
         self.token = None
+
+    def _connect_and_join(self):
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((self.dsuserver, PORT))
+
+        join_msg = ds_protocol.join(self.username, self.password)
+        client.sendall(join_msg.encode())
+
+        response = client.recv(4096).decode()
+        parsed = ds_protocol.extract_json(response)
+
+        if parsed.type == "ok":
+            self.token = parsed.token
+            return client
+
+        client.close()
+        return None
+
